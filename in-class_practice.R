f27b8e4c1e6dd68421060cf4096006bc1bae38a6
Angela Ying Xiao <angela.xiao@ndm.ox.ac.uk>
2025-03-27 11:58:08 +0000

    in-class practice

---
+++ in-class_practice.R
@@ -0,0 +1,73 @@
+# Examples of data cleaning of RWD ----
+
+
+# Load libraries ----
+library(openxlsx2)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(stringr)
+#library(tidyverse)
+
+
+## Read datasets needed ----
+sheet1 <- read_xlsx(file = "data/pop_death.xlsx", sheet = 1, start_row = 2)
+sheet2 <- read_xlsx(file = "data/pop_death.xlsx", sheet = 2, start_row = 2)
+
+## An alternative to lines 14,15 ----
+## for when there are multiple sheets, use one command to perform same task as above ----
+#for (i in 1:2) {
+  #assign(
+    #paste0("sheet", i), read_xlsx(file = "data/pop_death.xlsx", sheet = 1, start_row = 2))}
+
+
+## Clean and process the pop and death data ----
+#rbind(sheet1, sheet2) --> rbind() allows you to bind together vertically two sheets
+
+## Rename sheet column labels for rbinding ----
+sheet1 <- sheet1 %>% 
+  rename(age = Age) %>%
+  rename_with(
+    .fn = function(x) sub(pattern = "M_", replacement = "", x = x),
+    .cols = `2011_M_Pop`:`2021_M_Deaths`
+  )
+
+sheet2 <- sheet2 %>% 
+  rename(age = Age) %>%
+  rename_with(
+    .fn = function(x) sub(pattern = "F_", replacement = "", x = x),
+    .cols = `2011_F_Pop`:`2021_F_Deaths`
+  )
+
+## Check that sheet1 and sheet2 have same names ----
+identical(names(sheet1), names(sheet2))
+
+## Row bind sheet1 and sheet2 ----
+pop_death <- bind_rows(sheet1, sheet2, .id = "sex") %>%
+  mutate(sex = ifelse(sex == 1, "Male", "Female"))
+
+
+pop <- pop_death %>%
+  select(-paste(2011:2021, "Deaths", sep = "_")) %>%
+  pivot_longer(
+    cols = contains("Pop"),
+    names_to = "year", values_to = "pop"
+    )%>%
+  mutate(year = str_replace(year, "_Pop", ""))
+#mutate(year = sub(pattern = "_Pop", replacement = "", year))
+
+#select() selects columns, filter() selects rows
+death <- pop_death %>%
+  select(-paste(2011:2021, "Pop", sep = "_")) %>%
+  pivot_longer(
+    cols = contains("Deaths"),
+    names_to = "year", values_to = "death"
+    ) %>%
+  mutate(year = str_replace(year, "_Deaths", ""))
+
+pop_death <- left_join(pop, death) %>% 
+## Transform 'year' values from character to numeric ----
+  mutate(year = as.numeric(year))
+
+## Check that 'year' values have been changed from character to numeric ----
+  is.numeric(pop_death$year)
